8f82eb812c87a9ef6ebe277a6939b3a422ba4d57
363981044@qq.com <Heaven19740727>
2019-03-12 16:01:23 +0800

    场馆前端登录

--- src/main/java/cn/project/yoga/service/impl/VenueServiceImpl.java
+++ src/main/java/cn/project/yoga/service/impl/VenueServiceImpl.java
@@ -4,9 +4,10 @@ import cn.project.yoga.dao.VenueMapper;
 import cn.project.yoga.pojo.Venue;
 import cn.project.yoga.service.VenueService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class VenueServiceImpl implements VenueService {
     @Autowired
     private VenueMapper venueMapper;
