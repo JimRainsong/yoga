82d0c1630492adb7a33b82da94d7ee5bdcadbccd
Heaven19740727 <363981044@qq.com>
2019-03-12 16:23:40 +0800

    text controlt by cy

--- src/main/java/cn/project/yoga/controller/VenueUserControllt.java
+++ src/main/java/cn/project/yoga/controller/VenueUserControllt.java
@@ -1,5 +1,10 @@
 package cn.project.yoga.controller;
 
+import cn.project.yoga.utils.LayUiDataUtil;
+import cn.project.yoga.vo.LoginVo;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +13,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class VenueUserControllt {
 
     @RequestMapping("/loginVenue")
-    public String login() {
-        return "user/cyz/login";
+    public LayUiDataUtil login(LoginVo vo) {
+        Subject subject = SecurityUtils.getSubject();
+        if (!subject.isAuthenticated()) {
+            UsernamePasswordToken token = new UsernamePasswordToken(vo.getUserName(), vo.getPassword());
+            token.setRememberMe(true);
+            try {
+                subject.login(token);
+                return LayUiDataUtil.ok("登陆成功");
+            } catch (UnknownAccountException uae) {
+                return LayUiDataUtil.error("未知的用户类型");
+            } catch (IncorrectCredentialsException ice) {
+                return LayUiDataUtil.error("账号或者密码错误");
+            } catch (LockedAccountException lae) {
+                return LayUiDataUtil.error("账号已被锁定");
+            } catch (AuthenticationException ae) {
+                return LayUiDataUtil.error("账号或者密码错误");
+            } catch (Exception e) {
+                return LayUiDataUtil.error("人品问题，登录失败");
+            }
+        } else {
+            return LayUiDataUtil.error("干哈?!你都已经登陆成功了");
+        }
     }
 
     @RequestMapping("/registerVenue")
