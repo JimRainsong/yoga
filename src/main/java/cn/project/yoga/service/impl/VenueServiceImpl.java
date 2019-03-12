acf9d051b7ed9befd9fd506f7a3ed47b4e723729
JimRainsong <363981044@qq.com>
2019-03-12 15:53:09 +0800

    赵健男 3.12 第3次

--- src/main/java/cn/project/yoga/service/impl/VenueServiceImpl.java
+++ src/main/java/cn/project/yoga/service/impl/VenueServiceImpl.java
@@ -10,8 +10,5 @@ import java.util.List;
 public class VenueServiceImpl implements VenueService {
     @Autowired
     private VenueMapper venueMapper;
-    @Override
-    public List<Venue> selectAllVenue1() {
-        return venueMapper.selectAllVenue1();
-    }
+
 }
