707c0af3351d33ce2eafbc1bed3d89a303a4cede
NullPointExcption <625160751@qq.com>
2019-03-14 12:04:03 +0800

    朱俊陇(04)

--- src/main/java/cn/project/yoga/dao/AdMapper.java
+++ src/main/java/cn/project/yoga/dao/AdMapper.java
@@ -1,7 +1,9 @@
 package cn.project.yoga.dao;
 
 import cn.project.yoga.pojo.Ad;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -18,6 +20,35 @@ public interface AdMapper {
 
     int updateByPrimaryKey(Ad record);
 
-    @Select("select * from ad where examine=0 and flag=0")
-    List<Ad> selectAllAd1();
+    /**  zjl
+     * 模糊查询 + 分页 + 已认证或者未认证查询
+     * */
+    @Select("<script>"+
+    "select * from ad"+
+     "<where>"+
+            "<if test='adtitle !=null and \"\"!=adtitle'>"+
+            "and ad_title like '%' #{adtitle} '%'"+
+            "</if>"+
+            "<if test='datas ==0 '>"+
+            "and examine=#{datas}"+
+            "</if>"+
+            "<if test='datas ==1 '>"+
+            "and examine=#{datas}"+
+            "</if>"+
+            "</where>"+
+    "</script>")
+    List<Ad> selAllAdBylimitAndPage(@Param("currentPage") Integer currentPage, @Param("pageSize") Integer pageSize,@Param("datas") Integer datas, @Param("adtitle") String adtitle);
+
+    /**  zjl
+     * 根据广告id , 更改广告认证
+     *
+     * */
+    @Update("update ad set examine =#{val} where ad_id=#{daId}")
+    int updateStateByAdid(@Param("daId") Integer daId,@Param("val") Integer val);
+
+    /** zjl
+     * 获取最新的三条广告信息
+     * */
+    @Select("select * from ad ORDER BY ad_time desc limit 0,3")
+    List<Ad> selthreeads();
 }
--- src/main/java/cn/project/yoga/service/impl/ManagerServiceImpl.java
+++ src/main/java/cn/project/yoga/service/impl/ManagerServiceImpl.java
@@ -1,8 +1,10 @@
 package cn.project.yoga.service.impl;
 
 
+import cn.project.yoga.dao.AdMapper;
 import cn.project.yoga.dao.TeacherMapper;
 import cn.project.yoga.dao.VenueMapper;
+import cn.project.yoga.pojo.Ad;
 import cn.project.yoga.pojo.Teacher;
 import cn.project.yoga.pojo.Venue;
 import cn.project.yoga.service.ManagerService;
@@ -20,6 +22,10 @@ public class ManagerServiceImpl implements ManagerService {
 
     @Autowired
     private TeacherMapper teacherMapper;
+
+    @Autowired
+    private AdMapper adMapper;
+
     /*查询场馆信息表所有数据*/
     @Override
     public List<Venue> selAllVenues4_1(Integer currentPage,Integer pageSize,String venueName) {
@@ -38,5 +44,28 @@ public class ManagerServiceImpl implements ManagerService {
     @Override
     public List<Teacher> selAllTeacherByapprove(Integer currentPage, Integer pageSize, String teacherName) {
         return teacherMapper.selAllTeacherByName4_1(currentPage,pageSize,teacherName);
+    }
+
+    /** zjl
+     * 根据已认证和未认证的参数,分页和模糊查询*/
+    @Override
+    public List<Ad> selAllAdBylimit(Integer currentPage, Integer pageSize, Integer datas,String adtitle) {
+        return adMapper.selAllAdBylimitAndPage(currentPage,pageSize,datas,adtitle);
+    }
+
+    /**  zjl
+     * 根据广告id,更改广告认证
+     * */
+    @Override
+    public int updateAdByAdid(Integer daId, Integer val) {
+        return adMapper.updateStateByAdid(daId,val);
+    }
+
+    /** zjl
+     * 获取广告最新的三条
+     * */
+    @Override
+    public List<Ad> getadthree() {
+        return adMapper.selthreeads();
     }
 }
